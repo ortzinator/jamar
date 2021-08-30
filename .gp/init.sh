cbc2cc4c1b877d53fdab89224424cc612afb35bf
Brian Ortiz <ortzinator@gmail.com>
2021-08-30 20:05:52 +0000

    ?

--- .gp/init.sh
+++ .gp/init.sh
@@ -2,6 +2,6 @@
 
 bash .ddev/gitpod-setup-ddev.sh
 
-ddev composer install && gp sync-done composer
+ddev composer install && npm i && gp sync-done npm
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
 ddev exec "php artisan key:generate && php artisan migrate --seed"
