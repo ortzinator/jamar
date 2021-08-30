05d093c500ed5b104e6cb4d417ce74e984387dd6
Brian Ortiz <ortzinator@gmail.com>
2021-08-30 18:24:48 +0000

    testing

--- .gp/init.sh
+++ .gp/init.sh
@@ -2,7 +2,6 @@
 
 . gitpod-setup-ddev.sh
 
-ddev start
 ddev composer install
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
 ddev exec "php artisan key:generate"
