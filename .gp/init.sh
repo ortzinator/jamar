bd8838add235c6cb5da451d6d30e36ec724ebf52
Brian Ortiz <ortzinator@gmail.com>
2021-08-30 19:46:45 +0000

    Remove unused aliases and add vscode extensions

--- .gp/init.sh
+++ .gp/init.sh
@@ -5,5 +5,3 @@ bash .ddev/gitpod-setup-ddev.sh
 ddev composer install
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
 ddev exec "php artisan key:generate && php artisan migrate --seed"
-
-gp preview $(gp url 8080)
