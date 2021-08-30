4dc3627aa6f1a7c4c7fa3c4987ca8611e62ae0be
Brian Ortiz <ortzinator@gmail.com>
2021-08-30 18:33:19 +0000

    testing

--- .gp/init.sh
+++ .gp/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bash ../.ddev/gitpod-setup-ddev.sh
+bash .ddev/gitpod-setup-ddev.sh
 
 ddev composer install
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
