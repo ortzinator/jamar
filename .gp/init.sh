566c5287d5f239fe6c9b60276f68d019d3dbc75a
Brian Ortiz <ortzinator@gmail.com>
2021-09-09 18:39:53 +0000

    ddev

--- .gp/init.sh
+++ .gp/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-bash .gp/set-env.sh
-
 bash .ddev/gitpod-setup-ddev.sh
 
-ddev composer install && npm i && gp sync-done npm
+ddev composer install
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
 ddev exec "php artisan key:generate && php artisan migrate --seed"
+
+bash .gp/set-env.sh
--- .gp/set-env.sh
+++ .gp/set-env.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 url=$(gp url 8080)
-err_code=$?
-if [[ $url =~ ^https?:// ]];then
-    sed -i'' "s#^APP_URL=http://localhost*#APP_URL=$url\nASSET_URL=$url#g" .env
-    err_code=$?
-else
-    url="$(echo -e "$url" | head -1)"
-    log_silent -e "ERROR: malformed url: $url"
-fi
+
+sed -i'' "s|^APP_URL=http://web*|APP_URL=$url|g" .env
+sed -i'' "s|^ASSET_URL=http://web*|ASSET_URL=$url|g" .env
+
+cp .env .env.dusk.local
