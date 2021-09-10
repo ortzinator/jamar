73fc25043cf2f670f29add881a6e23206d0b7204
Brian Ortiz <ortzinator@gmail.com>
2021-09-10 19:51:47 +0000

    .

--- .gp/set-env.sh
+++ .gp/set-env.sh
@@ -2,7 +2,7 @@
 
 url=$(gp url 8080)
 
+cp .env .env.dusk.local
+
 sed -i'' "s|^APP_URL=http://web*|APP_URL=$url|g" .env
 sed -i'' "s|^ASSET_URL=http://web*|ASSET_URL=$url|g" .env
-
-cp .env .env.dusk.local
