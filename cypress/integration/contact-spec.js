1a113cb282fcef71394b221020bbfc4a906ce8d1
Ortzinator <ortzinator@gmail.com>
2021-09-14 21:09:14 -0500

    Added contact test

--- cypress/integration/contact-spec.js
+++ cypress/integration/contact-spec.js
@@ -3,5 +3,14 @@ describe('contacts', () => {
         cy.refreshDatabase().seed();
     });
 
-    context('user is an admin', () => {});
+    context('user is a manager', () => {
+        beforeEach(() => {
+            cy.login({ email: 'manager@jamar.com' });
+        });
+
+        it('can edit contacts', () => {
+            cy.visit('/contacts/1/edit');
+            cy.get('#address').invoke('val').should('not.be.empty');
+        });
+    });
 });
